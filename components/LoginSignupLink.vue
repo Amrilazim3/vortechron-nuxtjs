0dcd1a1133e7c907b9172e95fc7f76f1a910c4a7
Amrilazim3 <a.azim0711@gmail.com>
2022-01-26 22:22:26 +0800

    commit changes

--- components/LoginSignupLink.vue
+++ components/LoginSignupLink.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="space-x-4">
         <NuxtLink to="/auth/login" class="text-normal hover:text-blue-500" :class="{'text-blue-400' : this.$nuxt.$route.path == '/auth/login'}">
-            LOG IN
+            Sign In
         </NuxtLink>
         <NuxtLink to="/auth/signup" class="text-normal hover:text-blue-500" :class="{'text-blue-400' : this.$nuxt.$route.path == '/auth/signup'}">
-            SIGN UP
+            Sign Up
         </NuxtLink>
     </div>
 </template>
