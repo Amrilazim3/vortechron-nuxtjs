05887d5c1a4d727a69b0c85deb5d0e7954c01373
Amrilazim3 <a.azim0711@gmail.com>
2022-02-03 16:39:09 +0800

    placing the last content of post stay at bottom

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,12 +2,12 @@
     <section class="grid p-6 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
         <h1 class="text-4xl font-normal text-center my-8">Explore Blogs</h1>
         <div class="w-10/12 mx-auto sm:grid sm:grid-cols-6 sm:w-full">
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -21,17 +21,17 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
             </div>
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -45,17 +45,17 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
             </div>
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -69,19 +69,17 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
             </div>
-
-
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -95,17 +93,17 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
             </div>
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -119,17 +117,17 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
             </div>
-            <div class="px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
+            <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-200 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2">
                 <img src="https://picsum.photos/id/235/300/100" alt="" class="border rounded-md w-full">
                 <h5 class="my-4 text-xs text-blue-400">holiday</h5>
                 <h2 class="text-lg font-bold">Going  hiking by this weekend</h2>
-                <p class="my-4 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
-                <div class="flex justify-between">
+                <p class="my-4 pb-12 text-sm">ut atque fuga culpa, similique sequi cum eos quis dolorum.</p>
+                <div class="absolute bottom-0 w-full flex justify-between">
                     <div class="flex">
                         <div class="self-center">
                             <button class="bg-gray-300 border rounded-full p-1.5 mr-2">
@@ -143,7 +141,7 @@
                             <h5 class="text-xs font-normal text-gray-500">March 1, 2020</h5>
                         </div>
                     </div>
-                    <div class="self-center">
+                    <div class="self-center mr-4">
                         <button class="bg-blue-300 p-1.5 border rounded-2xl hover:bg-blue-400">Read more</button>
                     </div>
                 </div>
