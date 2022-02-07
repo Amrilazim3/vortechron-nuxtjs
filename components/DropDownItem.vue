0be9afeb3f92d5b5e040789baed828519b06e68c
Amrilazim3 <a.azim0711@gmail.com>
2022-02-07 23:15:53 +0800

    commit changes

--- components/DropDownItem.vue
+++ components/DropDownItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLink :to="to" class="px-1 hover:bg-gray-200 focus:bg-gray-400 rounded-md">
+    <NuxtLink :to="to" class="mt-1 px-1 hover:bg-gray-200 focus:bg-gray-400 rounded-md">
         <slot/>
     </NuxtLink>
 </template>
