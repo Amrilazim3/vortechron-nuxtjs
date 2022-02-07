0eeede9f3beb47fddf27f592e7aa050296a46b50
Amrilazim3 <a.azim0711@gmail.com>
2022-02-07 23:30:58 +0800

    file for authenticated user

---
+++ pages/user/account/edit-profile.vue
@@ -0,0 +1,135 @@
+<template>
+    <section class="grid md:flex p-6 mt-10 sm:max-w-xl sm:justify-center sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+        <div class="bg-white p-4 rounded-md shadow-2xl sm:p-6 sm:w-full lg:w-2/3">
+            <div>
+                <h3 class="text-2xl font-semibold">Edit Profile</h3>
+                <p class="text-sm">Update and secure your personal information</p>
+            </div>
+            <div class="bg-gray-300 h-0.5 mt-4"></div>
+            <form ref="form" @submit.prevent="edit">
+                <div class="mt-6 flex">
+                    <h2 class="w-1/3 self-center">Profile Image</h2>
+                    <div class="w-2/3">
+                        <template v-if="!this.$auth.user.image_url">
+                            <div class="bg-gray-300 max-w-max p-2.5 rounded-full">
+                                <img class="h-8" src="~/assets/default-profile-icon.svg" alt="">
+                            </div>
+                        </template>
+                        <template v-else>
+                            <img class="h-14 w-14 rounded-full" :src="this.form.image_full_url" id="image-url" alt="">
+                        </template>
+                        <input type="file" @change="handleFileUpload($event)" class="mt-4"/>
+                    </div>
+                </div>
+                <div class="mt-6 flex w-full">
+                    <h2 class="w-1/3 self-center">Name</h2>
+                    <div class="w-2/3"> 
+                        <input type="text" name="name" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.name" required/>
+                        <p v-if="errors.name" class="text-red-500">{{ errors.name[0] }}</p>
+                    </div> 
+                </div>
+                <div class="mt-6 flex w-full">
+                    <h2 class="w-1/3 self-center">Username</h2>
+                    <div class="w-2/3">
+                        <input type="text" name="username" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.username" required/>
+                        <p v-if="errors.username" class="text-red-500">{{ errors.username[0] }}</p>
+                    </div> 
+                </div>
+                <div class="mt-6 flex w-full">
+                    <h2 class="w-1/3 self-center">Bio</h2>
+                    <div class="w-2/3">
+                        <textarea rows="4" cols="30" name="bio" class="bg-white w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" placeholder="Enter your bio here" v-model="form.bio"/>
+                        <p v-if="errors.bio" class="text-red-500">{{ errors.bio[0] }}</p>
+                    </div> 
+                </div>
+                <div class="mt-6 flex w-full">
+                    <h2 class="w-1/3 self-center">Email</h2>
+                    <div class="w-2/3">
+                        <input type="email" name="email" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.email" required/>
+                        <p v-if="errors.email" class="text-red-500">{{ errors.email[0] }}</p>
+                    </div> 
+                </div>
+                <div class="flex justify-between">
+                    <div></div>
+                    <button class="mt-8 bg-blue-500 py-1.5 px-6 border-black rounded-md hover:bg-blue-600">
+                        Save
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="modal hidden" id="modal">
+            <div class="modal-content">
+                Update successful
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    mounted() {
+        this.form = JSON.parse(JSON.stringify(this.$auth.user))
+    },
+
+    data: () => ({
+        form: {},
+        file: '',
+        errors: []
+    }),
+
+    methods: {
+        async edit() {
+            try {
+                let formData = new FormData(this.$refs.form)
+                formData.append('file', this.file)
+                await this.$axios.$post('/api/user/profile/edit', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then((resp) => {
+                    document.getElementById('modal').classList.remove('hidden')
+                    document.addEventListener('click', this.removeModal)
+                    this.form = resp.user
+                    this.$auth.setUser(JSON.parse(JSON.stringify(resp.user)))
+                    this.errors = [];
+                })
+                .catch((err) => {
+                    if (err.response.status == 422) {
+                        this.errors = err.response.data.errors;
+                    }
+                })
+            } catch (error) {}
+        },
+
+        handleFileUpload(event) {
+            this.file = event.target.files[0];
+        },
+        
+        removeModal() {
+            document.getElementById('modal').classList.add('hidden');
+        }
+    }
+}
+</script>
+
+<style>
+.modal {
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.75);
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transition: all 0.3s;
+}
+.modal-content {
+    background-color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 2% 7%;
+    border-radius: 0.5rem;
+}
+</style>
---
+++ pages/user/account/profile.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="grid p-6 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+        <div class="md:flex md:justify-between">
+            <div class="flex mt-8">
+                <template v-if="this.$auth.user.image_url">
+                    <div>
+                        <img class="h-16 w-16 rounded-full" :src="this.$auth.user.image_full_url" alt="">
+                    </div>
+                </template>
+                <template v-else>
+                    <div class="bg-gray-300 p-2.5 rounded-full">
+                        <img class="h-12" src="~/assets/default-profile-icon.svg" alt="">
+                    </div>
+                </template>
+                <div class="ml-4">
+                    <h2 class="text-xl font-bold text-center">{{ this.$auth.user.username }}</h2>
+                    <NuxtLink to="/user/account/edit-profile" class="bg-blue-500 font-medium px-4 py-0.5 rounded-full hover:bg-blue-600">
+                        edit profile
+                    </NuxtLink>
+                </div>
+            </div>
+            <div class="font-semibold mt-3.5 space-x-3.5 text-xl md:-mt-0 md:self-center">
+                <span>0 follower</span>
+                <span>0 following</span>
+                <span>0 post</span>
+            </div>
+        </div>
+        <template v-if="this.$auth.user.bio == null">
+            <p class="mt-6 font-thin">No bio yet</p>
+        </template>
+        <template v-else>
+            <p class="mt-6">{{ this.$auth.user.bio }}</p>
+        </template>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
