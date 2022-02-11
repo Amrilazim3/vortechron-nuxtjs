796499b73a13178d9185b87c98570a9f5c01a4db
Amrilazim3 <a.azim0711@gmail.com>
2022-02-11 22:19:21 +0800

    commit changes

--- components/Input.vue
+++ components/Input.vue
@@ -4,7 +4,8 @@
         :id="id"
         class="bg-gray-300 w-full pl-4 h-10 mt-1.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" 
         :value="value"
-        @input="updateValue($event.target.value)">
+        @input="updateValue($event.target.value)"
+    >
 </template>
 
 <script>
