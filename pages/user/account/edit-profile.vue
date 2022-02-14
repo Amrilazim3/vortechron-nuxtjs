fa20712614041a816b481a164a4dc598eb601d7e
Amrilazim3 <a.azim0711@gmail.com>
2022-02-14 14:06:28 +0800

    adding vee-validate

--- pages/user/account/edit-profile.vue
+++ pages/user/account/edit-profile.vue
@@ -6,63 +6,73 @@
                 <p class="text-sm">Update and secure your personal information</p>
             </div>
             <div class="bg-gray-300 h-0.5 mt-4"></div>
-            <form ref="form" @submit.prevent="edit">
-                <div class="mt-6 flex">
-                    <h2 class="w-1/3 self-center">Profile Image</h2>
-                    <div class="w-2/3">
-                        <template v-if="!this.$auth.user.image_url">
-                            <div class="bg-gray-300 max-w-max p-2.5 rounded-full">
-                                <img class="h-8" src="~/assets/default-profile-icon.svg" alt="">
-                            </div>
-                        </template>
-                        <template v-else>
+            <ValidationObserver ref="formObserver">
+                <form ref="form" @submit.prevent="edit">
+                    <div class="mt-6 flex">
+                        <h2 class="w-1/3 self-center">Profile Image</h2>
+                        <!-- display user profile picture -->
+                        <div class="w-2/3">
+                            <template v-if="!this.form.image_url">
+                                <div class="bg-gray-300 max-w-max p-2.5 rounded-full">
+                                    <img class="h-8" src="~/assets/default-profile-icon.svg" alt="">
+                                </div>
+                            </template>
+                            <template v-else>
+                                <div class="flex">
+                                    <img class="h-14 w-14 rounded-full" :src="this.form.image_full_url" id="image-url" alt="">
+                                    <button class="ml-4 text-sm font-light underline hover:text-red-500" @click.prevent="removeFile">Remove</button>
+                                </div>
+                            </template>
                             <div class="flex">
-                                <img class="h-14 w-14 rounded-full" :src="this.form.image_full_url" id="image-url" alt="">
-                                <button class="ml-4 text-sm font-light underline hover:text-red-500" @click.prevent="removeFile">Remove</button>
+                                <input type="file" id="file" @change="handleFileUpload($event)" class="mt-4"/>
+                                <button class="self-end ml-2 py-0.5 px-2 border border-black bg-gray-200 hover:bg-gray-300" v-if="this.file" @click.prevent="clearFileForm">clear</button>
                             </div>
-                        </template>
-                        <div class="flex">
-                            <input type="file" id="file" @change="handleFileUpload($event)" class="mt-4"/>
-                            <button class="self-end ml-2 py-0.5 px-2 border border-black bg-gray-200 hover:bg-gray-300" v-if="this.file" @click.prevent="clearFileForm">clear</button>
                         </div>
-                        <p v-if="errors.file" class="text-red-500">{{ errors.file[0] }}</p>
                     </div>
-                </div>
-                <div class="mt-6 flex w-full">
-                    <h2 class="w-1/3 self-center">Name</h2>
-                    <div class="w-2/3"> 
-                        <input type="text" name="name" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.name" required/>
-                        <p v-if="errors.name" class="text-red-500">{{ errors.name[0] }}</p>
-                    </div> 
-                </div>
-                <div class="mt-6 flex w-full">
-                    <h2 class="w-1/3 self-center">Username</h2>
-                    <div class="w-2/3">
-                        <input type="text" name="username" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.username" required/>
-                        <p v-if="errors.username" class="text-red-500">{{ errors.username[0] }}</p>
-                    </div> 
-                </div>
-                <div class="mt-6 flex w-full">
-                    <h2 class="w-1/3 self-center">Bio</h2>
-                    <div class="w-2/3">
-                        <textarea rows="4" cols="30" name="bio" class="bg-white w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" placeholder="Enter your bio here" v-model="form.bio"/>
-                        <p v-if="errors.bio" class="text-red-500">{{ errors.bio[0] }}</p>
-                    </div> 
-                </div>
-                <div class="mt-6 flex w-full">
-                    <h2 class="w-1/3 self-center">Email</h2>
-                    <div class="w-2/3">
-                        <input type="email" name="email" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.email" required/>
-                        <p v-if="errors.email" class="text-red-500">{{ errors.email[0] }}</p>
-                    </div> 
-                </div>
-                <div class="flex justify-between">
-                    <div></div>
-                    <button class="mt-8 bg-blue-500 py-1.5 px-6 border-black rounded-md hover:bg-blue-600">
-                        Save
-                    </button>
-                </div>
-            </form>
+                    <div class="mt-6 flex w-full">
+                        <h2 class="w-1/3 self-center">Name</h2>
+                        <ValidationProvider vid="name" name="Name" rules="required|max:50" v-slot="{ errors }" class="w-2/3">
+                            <div> 
+                                <input type="text" name="name" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.name"/>
+                            </div>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </ValidationProvider>
+                    </div>
+                    <div class="mt-6 flex w-full">
+                        <h2 class="w-1/3 self-center">Username</h2>
+                        <ValidationProvider vid="username" name="Username" rules="required|min:4|max:15" v-slot="{ errors }" class="w-2/3">
+                            <div>
+                                <input type="text" name="username" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.username" />
+                            </div>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </ValidationProvider>
+                    </div>
+                    <div class="mt-6 flex w-full">
+                        <h2 class="w-1/3 self-center">Bio</h2>
+                        <ValidationProvider vid="bio" name="Bio" rules="max:100" v-slot="{ errors }" class="w-2/3">
+                            <div>
+                                <textarea rows="4" cols="30" name="bio" class="bg-white w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" placeholder="Enter your bio here" v-model="form.bio"/>
+                            </div>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </ValidationProvider>
+                    </div>
+                    <div class="mt-6 flex w-full">
+                        <h2 class="w-1/3 self-center">Email</h2>
+                        <ValidationProvider vid="email" name="Email" rules="required|email" v-slot="{ errors }" class="w-2/3">
+                            <div>
+                                <input type="email" name="email" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.email" />
+                            </div>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </ValidationProvider>
+                    </div>
+                    <div class="flex justify-between">
+                        <div></div>
+                        <button class="mt-8 bg-blue-500 py-1.5 px-6 border-black rounded-md hover:bg-blue-600">
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </ValidationObserver>
         </div>
         <div class="modal hidden" id="modal">
             <div class="modal-content">
@@ -91,12 +101,12 @@ export default {
     data: () => ({
         form: {},
         file: '',
-        errors: []
     }),
 
     methods: {
         async edit() {
             try {
+                let errors = [];
                 let formData = new FormData(this.$refs.form)
                 formData.append('file', this.file)
                 await this.$axios.$post('/api/user/profile/edit', formData, {
@@ -111,11 +121,11 @@ export default {
                     this.form = resp.user
                     this.$auth.setUser(JSON.parse(JSON.stringify(resp.user)))
                     this.clearFileForm()
-                    this.errors = [];
                 })
                 .catch((err) => {
                     if (err.response.status == 422) {
-                        this.errors = err.response.data.errors;
+                        errors = err.response.data.errors;
+                        this.$refs.formObserver.setErrors(errors)
                     }
                 })
             } catch (error) {}
