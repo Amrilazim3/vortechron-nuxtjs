4598449fc1467c5f7c8f44c067c96c9f57c4f94c
Amrilazim3 <a.azim0711@gmail.com>
2022-02-14 10:16:06 +0800

    adding vee-validate packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vee-validate.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -86,5 +87,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['vee-validate']
   }
 }
