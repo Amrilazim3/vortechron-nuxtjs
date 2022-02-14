d79e33bc850df7797c12ce18c099d53044045429
Amrilazim3 <a.azim0711@gmail.com>
2022-02-14 11:17:31 +0800

    implementing vee-validate form handling

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -7,29 +7,36 @@
             <p class="text-base text-blue-700 font-medium md:text-center lg:text-left">Continue exploring endlessly</p>
         </div>
         <div style="background-color: rgba(243, 243, 243, 1);" class="rounded-lg shadow-xl mt-6 lg:flex-1 xl:mr-8 2xl:ml-40 2xl:mr-14">
-            <form @submit.prevent="signIn" class="p-8">
-                <h3 class="text-2xl font-bold">Sign in to your account</h3>
-                <p class="text-base font-normal mt-4">Sign in with :</p>
-                <OAuthServices class="flex mt-2 space-x-4"/>
-                <div class="flex my-6">
-                    <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
-                    <p class="text-center mx-4 text-sm">Or manually sign in</p>
-                    <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
-                </div>
-                <div>
-                    <label for="email">Email</label>
-                    <Input type="email" id="email" v-model="form.email" required/> 
-                </div>
-                <div>
-                    <label for="password">Password</label>
-                    <Input type="password" id="password" v-model="form.password" required/> 
-                    <p v-if="errors" class="text-red-500">{{ errors }}</p>
-                </div>
-                <p class="mt-2">Don't have account? <NuxtLink to="/auth/sign-up" class="text-blue-400 hover:underline">Sign up</NuxtLink> here</p>
-                <BlueButton type="submit" class="mt-6 w-full transition duration-300">
-                    Sign In
-                </BlueButton>
-            </form>
+            <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+                <form @submit.prevent="handleSubmit(signIn)" class="p-8">
+                    <h3 class="text-2xl font-bold">Sign in to your account</h3>
+                    <p class="text-base font-normal mt-4">Sign in with :</p>
+                    <OAuthServices class="flex mt-2 space-x-4"/>
+                    <div class="flex my-6">
+                        <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
+                        <p class="text-center mx-4 text-sm">Or manually sign in</p>
+                        <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
+                    </div>
+                    <ValidationProvider vid="email" name="Email" rules="required|email" v-slot="{ errors }">
+                        <div>
+                            <label for="email">Email</label>
+                            <InputField type="email" id="email" v-model="form.email"/>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </div>
+                    </ValidationProvider>
+                    <ValidationProvider vid="password" name="Password" rules="required|min:9" v-slot="{ errors }">
+                        <div>
+                            <label for="password">Password</label>
+                            <InputField type="password" id="password" v-model="form.password"/>
+                            <p class="text-red-500">{{ errors[0] }}</p>
+                        </div>
+                    </ValidationProvider>
+                    <p class="mt-2">Don't have account? <NuxtLink to="/auth/sign-up" class="text-blue-400 hover:underline">Sign up</NuxtLink> here</p>
+                    <BlueButton type="submit" id="submit" class="mt-6 w-full transition duration-300">
+                        Sign In
+                    </BlueButton>
+                </form>
+            </ValidationObserver>
         </div>
     </section>
 </template>
@@ -53,17 +60,23 @@ export default {
             email: '',
             password: '',
         },
-
-        errors: '',
     }),
 
     methods: {
         async signIn() {
+            let submitButton = document.getElementById('submit');
+            submitButton.disabled = true;
+            submitButton.classList.add("bg-blue-800");
             try {
                 await this.$auth.loginWith('laravelSanctum', { data: this.form })
             } catch (err) {
+                submitButton.disabled = false;
+                submitButton.classList.remove("bg-blue-800");
+                console.log(err);
                 if (err.response.status = 422) {
-                    this.errors = 'Could not sign yout in with those credentials.'
+                    this.$refs.form.setErrors({
+                        password: ['Could not sign yout in with those credentials.']
+                    });
                 }
             }
         }
