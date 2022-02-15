ff3c835dcb39dc307f8fdad678d56d39c23d5a3f
Amrilazim3 <a.azim0711@gmail.com>
2022-02-15 10:18:11 +0800

    centering form

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="grid p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+    <section class="grid min-h-screen p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
         <div class="hidden md:block md:mt-10 lg:self-center lg:flex-1 xl:ml-8 2xl:ml-16">
             <h1 class="text-4xl text-black font-bold md:text-center lg:text-left">
                 WELCOME BACK
             </h1>
             <p class="text-base text-blue-700 font-medium md:text-center lg:text-left">Continue exploring endlessly</p>
         </div>
-        <div style="background-color: rgba(243, 243, 243, 1);" class="rounded-lg shadow-xl mt-6 lg:flex-1 xl:mr-8 2xl:ml-40 2xl:mr-14">
+        <div style="background-color: rgba(243, 243, 243, 1);" class="rounded-lg shadow-xl mt-6 lg:flex-1 lg:self-center xl:mr-8 2xl:ml-40 2xl:mr-14">
             <ValidationObserver ref="form" v-slot="{ handleSubmit }">
                 <form @submit.prevent="handleSubmit(signIn)" class="p-8">
                     <h3 class="text-2xl font-bold">Sign in to your account</h3>
@@ -17,19 +17,15 @@
                         <p class="text-center mx-4 text-sm">Or manually sign in</p>
                         <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
                     </div>
-                    <ValidationProvider vid="email" name="Email" rules="required|email" v-slot="{ errors }">
-                        <div>
-                            <label for="email">Email</label>
-                            <InputField type="email" id="email" v-model="form.email"/>
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="email" name="Email" rules="required|email" v-slot="{ errors }">
+                        <label for="email">Email</label>
+                        <InputField type="email" id="email" v-model="form.email"/>
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
-                    <ValidationProvider vid="password" name="Password" rules="required" v-slot="{ errors }">
-                        <div>
-                            <label for="password">Password</label>
-                            <InputField type="password" id="password" v-model="form.password"/>
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="password" name="Password" rules="required" v-slot="{ errors }">
+                        <label for="password">Password</label>
+                        <InputField type="password" id="password" v-model="form.password"/>
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
                     <p class="mt-2">Don't have account? <NuxtLink to="/auth/sign-up" class="text-blue-400 hover:underline">Sign up</NuxtLink> here</p>
                     <BlueButton type="submit" id="submit" class="mt-6 w-full transition duration-300">
--- pages/auth/sign-up.vue
+++ pages/auth/sign-up.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="grid p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+    <section class="grid min-h-screen p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
         <div class="hidden md:block md:mt-10 lg:self-center lg:flex-1 xl:ml-8 2xl:ml-16">
             <h1 class="text-4xl text-black font-bold md:text-center lg:text-left">
                 CREATE YOUR OWN BLOG POST
             </h1>
             <p class="text-base text-blue-700 font-medium md:text-center lg:text-left">Create your account now!</p>
         </div>
-        <div style="background-color: rgba(243, 243, 243, 1);" class="rounded-lg shadow-xl mt-6 lg:flex-1 xl:mr-8 2xl:ml-40 2xl:mr-14">
+        <div style="background-color: rgba(243, 243, 243, 1);" class="rounded-lg shadow-xl mt-6 lg:flex-1 lg:self-center xl:mr-8 2xl:ml-40 2xl:mr-14">
             <ValidationObserver ref="form" v-slot="{ handleSubmit }">
                 <form @submit.prevent="handleSubmit(signUp)" class="p-8">
                     <h3 class="text-2xl font-bold mb-4">Sign up to create account</h3>
@@ -17,40 +17,30 @@
                         <p class="text-center mx-4 text-sm">Or manually sign up</p>
                         <div class="bg-gray-300 h-0.5 flex-1 self-center"></div>
                     </div>
-                    <ValidationProvider vid="name" name="Name" rules="required|min:4|max:30" v-slot="{ errors }">
-                        <div>
-                            <label for="name">Name</label>
-                            <InputField type="text" id="name" v-model="form.name"/> 
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="name" name="Name" rules="required|min:4|max:30" v-slot="{ errors }">
+                        <label for="name">Name</label>
+                        <InputField type="text" id="name" v-model="form.name"/> 
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
-                    <ValidationProvider vid="username" name="Username" rules="required|min:4|max:15" v-slot="{ errors }">
-                        <div>
-                            <label for="username">Username</label>
-                            <InputField type="text" id="username" v-model="form.username"/> 
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="username" name="Username" rules="required|min:4|max:15" v-slot="{ errors }">
+                        <label for="username">Username</label>
+                        <InputField type="text" id="username" v-model="form.username"/> 
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
-                    <ValidationProvider vid="email" name="Email" rules="required|email" v-slot="{ errors }">
-                        <div>
-                            <label for="email">Email</label>
-                            <InputField type="email" id="email" v-model="form.email"/>
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="email" name="Email" rules="required|email" v-slot="{ errors }">
+                        <label for="email">Email</label>
+                        <InputField type="email" id="email" v-model="form.email"/>
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
-                    <ValidationProvider name="Password" rules="required|min:9" v-slot="{ errors }">
-                        <div>
-                            <label for="password">Password</label>
-                            <InputField type="password" id="password" v-model="form.password"/> 
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" name="Password" rules="required|min:9" v-slot="{ errors }">
+                        <label for="password">Password</label>
+                        <InputField type="password" id="password" v-model="form.password"/> 
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
-                    <ValidationProvider vid="password" name="Password Confirmation" rules="required|min:9" v-slot="{ errors }">
-                        <div>
-                            <label for="password_confirmation">Password confirmation</label>
-                            <InputField type="password" id="password_confirmation" v-model="form.password_confirmation"/> 
-                            <p class="text-red-500">{{ errors[0] }}</p>
-                        </div>
+                    <ValidationProvider tag="div" vid="password" name="Password Confirmation" rules="required|min:9" v-slot="{ errors }">
+                        <label for="password_confirmation">Password confirmation</label>
+                        <InputField type="password" id="password_confirmation" v-model="form.password_confirmation"/> 
+                        <p class="text-red-500">{{ errors[0] }}</p>
                     </ValidationProvider>
                     <p class="mt-2">Already sign up? <NuxtLink to="/auth/sign-in" class="text-blue-400 hover:underline">Sign in</NuxtLink> here</p>
                     <BlueButton type="submit" id="submit" class="mt-6 w-full transition duration-300">
