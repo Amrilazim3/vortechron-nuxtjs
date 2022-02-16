206848cf1587c8eaada2a1f0be966cacb1b61d9e
Amrilazim3 <a.azim0711@gmail.com>
2022-02-17 00:08:36 +0800

    hide this notifications from user that comes from social services

--- components/EmailVerificationNotice.vue
+++ components/EmailVerificationNotice.vue
@@ -1,21 +1,23 @@
 <template>
     <div v-if="this.$auth.loggedIn">
-        <div v-if="!this.$auth.user.email_verified_at" class="bg-red-200 py-2">
-            <h1 class="text-center font-medium text-lg">PLEASE VERIFIED YOUR ACCOUNT</h1>
-            <p class="text-center font-thin text-sm">You cannot create a post until you verify your email</p>
-            <p class="text-center font-thin text-sm">If you didn't receive email verification link, you can <button class="underline hover:text-blue-400" @click.prevent="resendEmailVerification">resend here</button></p>
-            <p class="text-center font-thin text-sm text-blue-500" v-if="verificationResend">Email verification link has been send! please check your email</p>
-        </div>
-        <div v-if="successVerified">
-            <div class="bg-green-400">
-                <p class="text-center font-thin text-sm">YOUR EMAIL IS VERIFIED!</p>
+        <template v-if="!this.$auth.user.service">
+            <div v-if="!this.$auth.user.email_verified_at" class="bg-red-200 py-2">
+                <h1 class="text-center font-medium text-lg">PLEASE VERIFIED YOUR ACCOUNT</h1>
+                <p class="text-center font-thin text-sm">You cannot create a post until you verify your email</p>
+                <p class="text-center font-thin text-sm">If you didn't receive email verification link, you can <button class="underline hover:text-blue-400" @click.prevent="resendEmailVerification">resend here</button></p>
+                <p class="text-center font-thin text-sm text-blue-500" v-if="verificationResend">Email verification link has been send! please check your email</p>
             </div>
-        </div>
-        <div v-if="alreadyVerified">
-            <div class="bg-green-400">
-                <p class="text-center font-thin text-sm">YOUR EMAIL ALREADY VERIFIED!</p>
+            <div v-if="successVerified">
+                <div class="bg-green-400">
+                    <p class="text-center font-thin text-sm">YOUR EMAIL IS VERIFIED!</p>
+                </div>
             </div>
-        </div>
+            <div v-if="alreadyVerified">
+                <div class="bg-green-400">
+                    <p class="text-center font-thin text-sm">YOUR EMAIL ALREADY VERIFIED!</p>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
