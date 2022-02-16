5139bf0767c7b6f7864c3a4eeb1f89cf257026ba
Amrilazim3 <a.azim0711@gmail.com>
2022-02-17 00:10:02 +0800

    change path name

--- components/ProfileSideBarNav.vue
+++ components/ProfileSideBarNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mb-10 md:mt-20">
-        <NuxtLink to="/user/account/edit-profile" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/edit-profile' ? 'text-blue-500 underline' : ''">Edit Profile</NuxtLink>
+        <NuxtLink to="/user/account/profile/edit-profile" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/profile/edit-profile' ? 'text-blue-500 underline' : ''">Edit Profile</NuxtLink>
         <br>
         <NuxtLink to="/user/account/change-password" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/change-password' ? 'text-blue-500 underline' : ''">Change Password</NuxtLink>
     </div>
