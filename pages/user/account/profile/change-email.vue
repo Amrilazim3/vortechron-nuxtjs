549af02e4cedbb11c625238bbb99c2b61c1ec562
Amrilazim3 <a.azim0711@gmail.com>
2022-02-17 00:13:16 +0800

    adding folder profile

---
+++ pages/user/account/profile/change-email.vue
@@ -0,0 +1,77 @@
+<template>
+    <section class="grid p-6 mt-10 sm:max-w-xl sm:mx-auto">
+        <div class="bg-white p-4 rounded-md shadow-2xl sm:p-6 sm:w-full">
+            <div>
+                <h3 class="text-2xl font-semibold text-center">Change Email</h3>
+            </div>
+            <ValidationObserver ref="form" tag="div" v-slot="{ handleSubmit }">
+                <form @submit.prevent="handleSubmit(changeEmail)">
+                    <div class="mt-6">
+                        <h2>Current Email</h2>
+                        <p class="select-none bg-white h-8 w-full pl-2.5 py-0.5 border border-gray-400 text-gray-500 rounded-md">{{ currentEmail }}</p>
+                    </div>
+                    <ValidationProvider tag="div" vid="email" name="Email" rules="required|email" v-slot="{ errors }" class="mt-6">
+                        <h2>New Email</h2>
+                        <input type="email" class="bg-white h-8 w-full pl-2.5 border border-gray-400 rounded-md focus:border-gray-600 focus:outline-none" placeholder="Enter your new email" v-model="form.email">
+                        <p name="error-message" class="text-red-500">{{ errors[0] }}</p>
+                    </ValidationProvider>
+                    <ValidationProvider tag="div" vid="password" name="Password" rules="required" v-slot="{ errors }" class="mt-6">
+                        <h2>Password</h2>
+                        <input type="password" class="bg-white h-8 w-full pl-2.5 border border-gray-400 rounded-md focus:border-gray-600 focus:outline-none" placeholder="Enter your password" v-model="form.password">
+                        <p name="error-message" class="text-red-500">{{ errors[0] }}</p>
+                    </ValidationProvider>
+                    <div>
+                        <BlueButton type="submit" class="mt-4 w-full">Save</BlueButton>
+                    </div>
+                </form>
+            </ValidationObserver>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+    middleware: 'auth',
+
+    head: {
+        title: '| Change Email',
+        meta: [
+            { hid: 'description', name: 'description', content: 'Bookmarked' }
+        ],
+    },
+
+    mounted() {
+        // if (this.$auth.user.service) {
+        //     this.getSocialServicePassword();
+        // }
+    },
+
+    data() {
+        return {
+            currentEmail: this.$auth.user.email,
+            form: {
+                email: '',
+                password: ''
+            },
+        }
+    },
+
+    methods: {
+        changeEmail() {
+            console.log(this.form);
+        },
+
+        getSocialServicePassword() {
+            console.log('check social service has password or not');
+            this.$axios.$get('/api/user/account/profile/change-email/get-sosial-service-password/' + this.$auth.user.id)
+                .then((resp) => {
+                    console.log(resp);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+    }
+}
+</script>
