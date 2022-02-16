4237893f89d5187111ebdf5c61a3b9a0f23a81d7
Amrilazim3 <a.azim0711@gmail.com>
2022-02-16 11:06:59 +0800

    adding path to settings page

--- components/ProfileDropDown.vue
+++ components/ProfileDropDown.vue
@@ -28,14 +28,9 @@
         <template>
             <div v-if="openProfile" class="absolute z-50 flex flex-col w-40 py-2 px-1 mt-2 overflow-auto bg-white rounded-xl max-h-28 sm:max-h-36">
                 <DropDownItem to="/user/account/profile" class="-mt-0" :class="{'bg-gray-300' : this.$nuxt.$route.path == '/user/account/profile'}">Profile</DropDownItem>
-                <DropDownItem to="/user/account/edit-profile" class="ml-4 text-sm font-normal" :class="{'bg-gray-300' : this.$nuxt.$route.path == '/user/account/edit-profile'}">
-                    Edit Profile
-                </DropDownItem>
-                <DropDownItem to="/user/account/change-password" class="ml-4 text-sm font-normal" :class="{'bg-gray-300' : this.$nuxt.$route.path == '/user/account/change-password'}">
-                    Change Password
-                </DropDownItem>
                 <DropDownItem to="/user/liked" :class="{'bg-gray-300' : this.$nuxt.$route.path == '/user/liked'}">Liked</DropDownItem>
                 <DropDownItem to="/user/bookmarked" :class="{'bg-gray-300' : this.$nuxt.$route.path == '/user/bookmarked'}">Bookmarked</DropDownItem>
+                <DropDownItem to="/user/account/edit-profile" :class="{ 'bg-gray-300' : this.$nuxt.$route.path == '/user/account/edit-profile', 'bg-gray-300' : this.$nuxt.$route.path == '/user/account/change-password' }">Settings</DropDownItem>
                 <button class="flex px-1 mt-4 hover:text-red-500" @click="signOut">sign out</button>
             </div>
         </template>
