f0e753826542976e6e7d37efa4569802e14a278f
Amrilazim3 <a.azim0711@gmail.com>
2022-02-17 22:28:16 +0800

    adding custom functions in plugins array

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vee-validate.js'
+    '~/plugins/vee-validate.js',
+    '~/plugins/form/disable-button.js',
+    '~/plugins/form/undisable-button.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
