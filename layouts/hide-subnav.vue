994a9c24c35daa48d909f27778816e10a4b03425
Amrilazim3 <a.azim0711@gmail.com>
2022-02-18 18:10:25 +0800

    making layout for hiding sub navigation bar

---
+++ layouts/hide-subnav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="background-color: rgba(202, 202, 202, 1);" class="min-h-screen">
+        <Header />
+        <EmailVerificationNotice />
+        
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
