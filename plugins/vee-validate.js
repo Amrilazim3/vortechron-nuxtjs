bbc940c05334c4adc39a7004158875984ceba296
Amrilazim3 <a.azim0711@gmail.com>
2022-02-19 10:26:20 +0800

    import regex rules

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -22,7 +22,15 @@ extend("email", email);
 
 extend("min", min);
 
-extend("max", max)
+extend("max", max);
+
+extend("verify_password", {
+    validate: value => {
+        var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!\?@#\$%\^&\*\.\-_])(?=.{9,})");
+        return strongRegex.test(value);
+    },
+    message: 'The password must contain at least: 1 uppercase letter, 1 lowercase letter, 1 number, and one special character (E.g. , . _ & ? etc).'
+});
 
 localize(dictionary);
 setInteractionMode('eager');
