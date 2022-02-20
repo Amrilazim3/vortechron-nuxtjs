e8349747f7ede1b2836f9e1365f0116ceac6b263
Amrilazim3 <a.azim0711@gmail.com>
2022-02-20 23:00:22 +0800

    commit changes

--- pages/auth/sign-up.vue
+++ pages/auth/sign-up.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="grid min-h-screen p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
-        <div class="hidden md:block md:mt-10 lg:self-center lg:flex-1 xl:ml-8 2xl:ml-16">
+    <section class="min-h-screen p-10 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:flex lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+        <div class="block md:mt-10 lg:self-center lg:flex-1 xl:ml-8 2xl:ml-16">
             <h1 class="text-4xl text-black font-bold md:text-center lg:text-left">
                 CREATE YOUR OWN BLOG POST
             </h1>
