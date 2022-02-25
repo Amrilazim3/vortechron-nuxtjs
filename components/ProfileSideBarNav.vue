2860922c728e5b6dc511d62b2037f67129856986
Amrilazim3 <a.azim0711@gmail.com>
2022-02-25 11:31:05 +0800

    commit changes

--- components/ProfileSideBarNav.vue
+++ components/ProfileSideBarNav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mb-10 md:mt-20">
-        <NuxtLink to="/user/account/profile/edit-profile" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/profile/edit-profile' ? 'text-blue-500 underline' : ''">Edit Profile</NuxtLink>
+        <NuxtLink to="/user/account/profile/edit-profile" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/profile/edit-profile' ? 'text-blue-500' : ''">Edit Profile</NuxtLink>
         <br>
-        <NuxtLink to="/user/account/change-password" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/change-password' ? 'text-blue-500 underline' : ''">Change Password</NuxtLink>
+        <NuxtLink to="/user/account/change-password" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/change-password' ? 'text-blue-500' : ''">Change Password</NuxtLink>
     </div>
 </template>
