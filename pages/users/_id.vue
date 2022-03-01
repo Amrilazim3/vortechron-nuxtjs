a13ed391932a3165b83a5e22de7a17123b80f39b
Amrilazim3 <a.azim0711@gmail.com>
2022-03-01 17:44:47 +0800

    page for viewing other user in application

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -12,52 +12,34 @@
                         <img class="h-12" src="~/assets/default-profile-icon.svg" alt="">
                     </div>
                 </template>
-                <template v-if="this.$auth.loggedIn">
-                    <div class="ml-4">
-                        <h2 class="text-xl font-bold text-center">{{ user.username }}</h2>
-                        <template v-if="user.isSentFriendRequestFrom">
-                            <p class="text-sm font-light">this user wants to follow you</p>
-                            <button class="bg-blue-500 px-4 rounded-md hover:text-white" @click.prevent="acceptRequest(user.id)">
-                                Accept
-                            </button>
-                            <button class="bg-red-500 px-4 rounded-md hover:text-white" @click.prevent="denyRequest(user.id)">
-                                Ignore
-                            </button>
-                        </template>
-                        <template v-else-if="user.isSentFriendRequestTo">
-                            <button class="bg-gray-400 px-4 rounded-md hover:text-white" @click.prevent="cancelRequest(user.id)">
-                                Requested
-                            </button>
-                        </template>
-                        <template v-else-if="user.isFriendWith">
-                            <button class="bg-blue-500 px-4 hover:text-red-500 rounded-md" @click.prevent="unFollow(user.id)">
-                                Following
-                            </button>
-                        </template>
-                        <template v-else-if="!user.isFriendWith && user.isNotFollowBack">
-                            <button class="bg-blue-500 px-4 hover:text-white rounded-md" @click.prevent="follow(user.id)">
-                                Follow Back
-                            </button>
-                        </template>
-                        <template v-else-if="!user.isFriendWith">
-                            <button class="bg-blue-500 hover:text-white px-4 rounded-md text-black" @click.prevent="follow(user.id)">
-                                Follow
-                            </button>
-                        </template>
-                    </div>
-                </template>
-                <template v-else>
-                    <div class="ml-4">
-                        <h2 class="text-xl font-bold text-center">{{ user.username }}</h2>
-                        <button class="bg-blue-500 hover:text-white px-4 rounded-md text-black" @click="$router.push('/auth/sign-in')">
-                            Sign in to follow
+                <div class="ml-4" v-if="$auth.loggedIn">
+                    <h2 class="text-xl font-bold text-center">{{ user.username }}</h2>
+                    <template v-if="!user.isFriendWith && user.isFriendWithAuthUser">
+                        <button class="bg-blue-500 px-4 hover:text-white rounded-md" @click.prevent="follow(user.id)">
+                            Follow Back
                         </button>
-                    </div>
-                </template>
+                    </template>
+                    <template v-else-if="user.isFriendWith">
+                        <button class="bg-blue-500 px-4 hover:text-red-500 rounded-md" @click.prevent="unFollow(user.id)">
+                            Following
+                        </button>
+                    </template>
+                    <template v-else-if="!user.isFriendWith">
+                        <button class="bg-blue-500 hover:text-white px-4 rounded-md text-black" @click.prevent="follow(user.id)">
+                            Follow
+                        </button>
+                    </template>
+                </div>
+                <div class="ml-4" v-else>
+                    <h2 class="text-xl font-bold text-center">{{ user.username }}</h2>
+                    <button class="bg-blue-500 px-4 hover:text-white rounded-md" @click="$router.push('/auth/sign-in')">
+                        Follow
+                    </button>
+                </div>
             </div>
             <div class="font-semibold mt-3.5 space-x-3.5 text-xl md:-mt-0 md:self-center hidden md:block">
-                <span>{{ followers }} followers</span>
-                <span>{{ following }} following</span>
+                <NuxtLink :to="`/users/followers/${user.id}`">{{ followers }} followers</NuxtLink>
+                <NuxtLink :to="`/users/following/${user.id}`">{{ following }} following</NuxtLink>
                 <span>0 post</span>
             </div>
         </div>
@@ -71,12 +53,13 @@
             </template>
         </div>
         <div class="font-semibold mt-3.5 space-x-3.5 text-xl md:-mt-0 md:self-center md:hidden">
-            <span>{{ followers }} followers</span>
-            <span>{{ following }} following</span>
+            <NuxtLink :to="`/users/followers/${user.id}`">{{ followers }} followers</NuxtLink>
+            <NuxtLink :to="`/users/following/${user.id}`">{{ following }} following</NuxtLink>
             <span>0 post</span>
         </div>
     </section>
 </template>
+
 
 <script>
 export default {
@@ -129,34 +112,21 @@ export default {
 
                     if (res.is_friend_with == 1 && res.user_friend_with_auth_user == 1) {
                         this.user.isFriendWith = true;
+                        this.user.isFriendWithAuthUser = true;
                         this.user.isNotFollowBack = false;
-                        console.log('first condition');
                     } else if (res.is_friend_with == 0 && res.user_friend_with_auth_user == 1) {
                         this.user.isFriendWith = false;
+                        this.user.isFriendWithAuthUser = true;
                         this.user.isNotFollowBack = true;
-                        console.log('second condition');
                     } else if (res.is_friend_with == 1 && res.user_friend_with_auth_user == 0) {
                         this.user.isFriendWith = true;
+                        this.user.isFriendWithAuthUser = false;
                         this.user.isNotFollowBack = false;
-                        console.log('third condition');
                     }  else if (res.is_friend_with == 0 && res.user_friend_with_auth_user == 0) {
                         this.user.isFriendWith = false;
+                        this.user.isFriendWithAuthUser = false;
                         this.user.isNotFollowBack = false;
-                        console.log('fourth conditon');
                     }
-
-                    if (res.is_sent_friend_request_to == 1) {
-                        this.user.isSentFriendRequestTo = true;
-                    } else {
-                        this.user.isSentFriendRequestTo = false;
-                    }
-                    if (res.is_sent_friend_request_from == 1) {
-                        this.user.isSentFriendRequestFrom = true;
-                    } else {
-                        this.user.isSentFriendRequestFrom = false;
-                    }
-
-                    console.log(this.user);
                 })
                 .catch((err) => {
                     if (err.response.status == 404) {
@@ -165,62 +135,29 @@ export default {
                 })
         },
 
-        acceptRequest(id) {
-            this.$axios.$patch(`/api/users/accept/${id}`)
-                .then(() => {
-                    let user = this.user;
-                    this.user = {};
-                    user.isSentFriendRequestFrom = false;
-                    if (!user.isFriendWith) {
-                        user.isNotFollowBack = true;  
-                    } 
-                    this.user = user;
-                    this.following += 1;
-                })
-        },
-
-        denyRequest(id) {
-            this.$axios.$patch(`/api/users/deny/${id}`)
-                .then(() => {
-                    let user = this.user;
-                    this.user = {};
-                    user.isSentFriendRequestFrom = false;
-                    this.user = user;
-                })
-        },
-
         follow(id) {
             this.$axios.$post(`/api/users/${id}`)
                 .then(() => {
+                    this.followers += 1;
                     let user = this.user;
                     this.user = {};
-                    user.isSentFriendRequestTo = true;
-                    this.user = user;
-                })
-        },
-
-        cancelRequest(id) {
-            this.$axios.$delete(`/api/users/cancel-request/${id}`)
-                .then(() => {
-                    let user = this.user;
-                    this.user = {};
-                    user.isSentFriendRequestTo = false;
+                    user.isFriendWith = true;
                     this.user = user;
                 })
         },
 
         unFollow(id) {
-            let answer = confirm("If you unfollow this user, you need to make request again to be a friend.");
-
-            if (answer) {
-                this.$axios.$delete(`/api/users/unfollow/${id}`)
-                    .then(() => {
-                        let user = this.user;
-                        this.user = {};
-                        user.isFriendWith = false;
-                        this.user = user;
-                    })
-            }
+            this.$axios.$delete(`/api/users/unfollow/${id}`)
+                .then(() => {
+                    this.followers -= 1;
+                    let user = this.user;
+                    this.user = {};
+                    user.isFriendWith = false;
+                    if (this.isFriendWithAuthUser) {
+                        user.isNotFollowBack = true;
+                    }
+                    this.user = user;
+                })
         },
 
     }
