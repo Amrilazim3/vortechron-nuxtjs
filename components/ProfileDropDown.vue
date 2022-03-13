c63404affe5fc8f690d0ffda6e12df6f006c207a
Amrilazim3 <a.azim0711@gmail.com>
2022-03-13 09:50:45 +0800

    commit changes

--- components/ProfileDropDown.vue
+++ components/ProfileDropDown.vue
@@ -29,23 +29,23 @@
             <div v-if="openProfile" class="absolute z-50 flex flex-col w-40 py-2 px-1 mt-2 overflow-auto bg-white rounded-xl max-h-28 sm:max-h-36">
                 <div class="relative flex hover:bg-gray-200 focus:bg-gray-400 rounded-md">
                     <DropDownItem to="/user/account/profile" :class="{'bg-gray-300 hover:bg-gray-200 rounded-md' : this.$nuxt.$route.path == '/user/account/profile'}">Profile</DropDownItem>
-                    <img src="~/assets/profile-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-14 cursor-pointer" @click="$router.push('/user/account/profile')">
+                    <img src="~/assets/profile-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-14 cursor-pointer opacity-50" @click="$router.push('/user/account/profile')">
                 </div>
                 <div class="relative flex hover:bg-gray-200 focus:bg-gray-400 rounded-md">
                     <DropDownItem to="/user/friends" :class="{'bg-gray-300 hover:bg-gray-200 rounded-md' : this.$nuxt.$route.path == '/user/friends'}">Friends</DropDownItem>
-                    <img src="~/assets/friends-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-16 cursor-pointer" @click="$router.push('/user/friends')">
+                    <img src="~/assets/friends-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-16 cursor-pointer opacity-50" @click="$router.push('/user/friends')">
                 </div>
                 <div class="relative flex hover:bg-gray-200 focus:bg-gray-400 rounded-md">
                     <DropDownItem to="/user/liked" :class="{'bg-gray-300 hover:bg-gray-200 rounded-md' : this.$nuxt.$route.path == '/user/liked'}">Liked</DropDownItem>
-                    <img src="~/assets/heart-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-12 cursor-pointer" @click="$router.push('/user/liked')">
+                    <img src="~/assets/heart-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-12 cursor-pointer opacity-50" @click="$router.push('/user/liked')">
                 </div>
                 <div class="relative flex hover:bg-gray-200 focus:bg-gray-400 rounded-md">
                     <DropDownItem to="/user/bookmarked" :class="{'bg-gray-300 hover:bg-gray-200 rounded-md' : this.$nuxt.$route.path == '/user/bookmarked'}">Bookmarked</DropDownItem>
-                    <img src="~/assets/bookmark-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-24 pl-1.5 cursor-pointer" @click="$router.push('/user/bookmarked')">
+                    <img src="~/assets/bookmark-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-24 pl-1.5 cursor-pointer opacity-50" @click="$router.push('/user/bookmarked')">
                 </div>
                 <div class="relative flex hover:bg-gray-200 focus:bg-gray-400 rounded-md">
                     <DropDownItem to="/user/account/profile/edit-profile" :class="{ 'bg-gray-300 hover:bg-gray-200 rounded-md' : this.$nuxt.$route.path == '/user/account/profile/edit-profile' || this.$nuxt.$route.path == '/user/account/change-password' }">Settings</DropDownItem>
-                    <img src="~/assets/gear-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-16 pl-1 cursor-pointer" @click="$router.push('/user/bookmarked')">
+                    <img src="~/assets/gear-logo.svg" alt="" class="h-4 absolute mt-1.5 ml-16 pl-1 cursor-pointer opacity-50" @click="$router.push('/user/bookmarked')">
                 </div>
 
                 <button class="flex px-1 mt-4 hover:text-red-500" @click="signOut">sign out</button>
