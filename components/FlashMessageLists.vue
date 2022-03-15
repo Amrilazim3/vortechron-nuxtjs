6c847b78e96fbf9e67b3f81b916682270f1bd4b1
Amrilazim3 <a.azim0711@gmail.com>
2022-03-15 15:11:44 +0800

    list all notification message

---
+++ components/FlashMessageLists.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="z-30 fixed bottom-14 right-1.5">
+        <FlashMessage v-for="notification in notifications" :key="notification.id" :notification="notification"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        notifications: {
+            type: Array
+        }
+    }
+}
+</script>
