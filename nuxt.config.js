3cd22518b532e2cf8fa42948bf4d09e226b3cef1
Amrilazim3 <a.azim0711@gmail.com>
2022-03-17 11:58:57 +0800

    commit changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,95 +1,97 @@
 const API_URL = 'https://api.vortechron.com'
 
 export default {
-  ssr: false,
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: '',
-    titleTemplate: 'VORTECHRON %s',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  loading: {
-    color: 'rgb(14 165 233)',
-    height: '5px'
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/vee-validate.js',
-    '~/plugins/form/disable-button.js',
-    '~/plugins/form/undisable-button.js',
-    '~/plugins/vue-observe-visibility'
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next'
-  ],
-
-  auth: {
-    strategies: {
-      'laravelSanctum': {
-        provider: 'laravel/sanctum',
-        url: API_URL,
-        endpoints: {
-          login: {
-            url: '/api/sign-in'
-          },
-          logout: {
-            url: '/api/sign-out'
-          },
-          user: {
-            url: '/api/user'
-          }
+    ssr: false,
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: '',
+        titleTemplate: 'VORTECHRON %s',
+        htmlAttrs: {
+        lang: 'en'
         },
-        user: {
-          property: false
-        }
-      }
+        meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    redirect: {
-      login: '/auth/sign-in',
-      logout: '/',
-      home: '/dashboard'
+
+    loading: {
+        color: 'rgb(14 165 233)',
+        height: '5px'
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+    ],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        '~/plugins/vee-validate.js',
+        '~/plugins/form/disable-button.js',
+        '~/plugins/form/undisable-button.js',
+        '~/plugins/vue-observe-visibility'
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        '@nuxtjs/auth-next'
+    ],
+
+    auth: {
+        strategies: {
+        'laravelSanctum': {
+            provider: 'laravel/sanctum',
+            url: API_URL,
+            endpoints: {
+            login: {
+                url: '/api/sign-in'
+            },
+            logout: {
+                url: '/api/sign-out'
+            },
+            user: {
+                url: '/api/user'
+            }
+            },
+            user: {
+            property: false
+            }
+        }
+        },
+        redirect: {
+        login: '/auth/sign-in',
+        logout: '/',
+        home: '/dashboard'
+        }
+    },
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: API_URL,
+        credentials: true
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: [
+            'vee-validate'
+        ]
     }
-  },
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: API_URL,
-    credentials: true
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: ['vee-validate']
-  }
 }
