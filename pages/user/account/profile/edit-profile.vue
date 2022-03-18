8e8d076bc7eea6d0ec9860db0bb557c73fa9adb5
Amrilazim3 <a.azim0711@gmail.com>
2022-03-18 17:31:07 +0800

    commit tailwind auto format styling

--- pages/user/account/profile/edit-profile.vue
+++ pages/user/account/profile/edit-profile.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <div class="bg-green-400 hidden" id="password_set_message">
-            <p class="text-center font-thin text-sm">Password has been set.</p>
+        <div class="hidden bg-green-400" id="password_set_message">
+            <p class="text-sm font-thin text-center">Password has been set.</p>
         </div>
         <section class="grid p-6 mt-10 sm:max-w-xl sm:mx-auto md:flex md:justify-around md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
             <ProfileSideBarNav />
-            <div class="bg-white p-4 rounded-md shadow-2xl sm:p-6 sm:w-full lg:w-2/3">
+            <div class="p-4 bg-white rounded-md shadow-2xl sm:p-6 sm:w-full lg:w-2/3">
                 <div>
                     <h3 class="text-2xl font-semibold">Edit Profile</h3>
                     <p class="text-sm">Update and secure your personal information</p>
                 </div>
                 <div class="bg-gray-300 h-0.5 mt-4"></div>
-                    <div class="mt-6 flex">
-                        <h2 class="w-1/3 self-center">Profile Image</h2>
+                    <div class="flex mt-6">
+                        <h2 class="self-center w-1/3">Profile Image</h2>
                         <div class="w-2/3">
                             <template v-if="!this.form.image_url">
                                 <template v-if="!this.file">
@@ -22,25 +22,25 @@
                                 </template>
                                 <template v-else>
                                     <div>
-                                        <img id="output-selected-file" :src="this.selectedImage" class="h-14 w-14 rounded-full" />
+                                        <img id="output-selected-file" :src="this.selectedImage" class="rounded-full h-14 w-14" />
                                     </div>
                                 </template>
                             </template>
                             <template v-else>
                                 <template v-if="!this.file">
                                     <div class="flex">
-                                        <img class="h-14 w-14 rounded-full" :src="this.form.image_full_url" id="image-url">
+                                        <img class="rounded-full h-14 w-14" :src="this.form.image_full_url" id="image-url">
                                         <button class="ml-4 hover:underline hover:text-red-500" @click.prevent="removeFile">Remove</button>
                                     </div>
                                 </template>
                                 <template v-else>
                                     <div>
-                                        <img id="output-selected-file" :src="this.selectedImage" class="h-14 w-14 rounded-full" />
+                                        <img id="output-selected-file" :src="this.selectedImage" class="rounded-full h-14 w-14" />
                                     </div>
                                 </template>
                             </template>
                             <div>
-                                <label for="file" class="bg-blue-400 cursor-pointer hover:bg-blue-500 inline-block mt-4 px-2 py-1 rounded-md">
+                                <label for="file" class="inline-block px-2 py-1 mt-4 bg-blue-400 rounded-md cursor-pointer hover:bg-blue-500">
                                     <input type="file" id="file" name="file" accept="image/*" @change="handleFileUpload($event)" class="hidden"/>
                                     Select Photo
                                 </label>
@@ -50,29 +50,29 @@
                     </div>
                     <ValidationObserver ref="formObserver" tag="div" v-slot="{ handleSubmit }">
                         <form ref="form" @submit.prevent="handleSubmit(edit)">
-                            <div class="mt-6 flex w-full">
-                                <h2 class="w-1/3 self-center">Name</h2>
+                            <div class="flex w-full mt-6">
+                                <h2 class="self-center w-1/3">Name</h2>
                                 <ValidationProvider tag="div" vid="name" name="Name" rules="required|max:50" v-slot="{ errors }" class="w-2/3">
                                     <input type="text" id="name" name="name" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.name"/>
                                     <p name="error-message" class="text-red-500">{{ errors[0] }}</p>
                                 </ValidationProvider>
                             </div>
-                            <div class="mt-6 flex w-full">
-                                <h2 class="w-1/3 self-center">Username</h2>
+                            <div class="flex w-full mt-6">
+                                <h2 class="self-center w-1/3">Username</h2>
                                 <ValidationProvider tag="div" vid="username" name="Username" rules="required|min:4|max:15" v-slot="{ errors }" class="w-2/3">
                                     <input type="text" id="username" name="username" class="bg-white h-8 w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" v-model="form.username" />
                                     <p name="error-message" class="text-red-500">{{ errors[0] }}</p>
                                 </ValidationProvider>
                             </div>
-                            <div class="mt-6 flex w-full">
-                                <h2 class="w-1/3 self-center">Bio</h2>
+                            <div class="flex w-full mt-6">
+                                <h2 class="self-center w-1/3">Bio</h2>
                                 <ValidationProvider tag="div" vid="bio" name="Bio" rules="max:100" v-slot="{ errors }" class="w-2/3">
                                     <textarea rows="4" cols="30" id="bio" name="bio" class="bg-white w-full pl-2.5 border border-gray-400 focus:border-gray-600 focus:outline-none rounded-md" placeholder="Enter your bio here" v-model="form.bio"/>
                                     <p name="error-message" class="text-red-500">{{ errors[0] }}</p>
                                 </ValidationProvider>
                             </div>
-                            <div class="mt-6 flex w-full cursor-default">
-                                <h2 class="w-1/3 self-center">
+                            <div class="flex w-full mt-6 cursor-default">
+                                <h2 class="self-center w-1/3">
                                     Email
                                     <template v-if="form.email_verified_at || form.service">
                                         <span class="text-sm font-light text-blue-400">(verified)</span>
@@ -86,7 +86,7 @@
                                     <NuxtLink to="/user/account/profile/change-email" class="text-sm text-blue-400 ml-2.5 self-end hover:underline cursor-pointer">change</NuxtLink>
                                 </template>
                                 <template v-else>
-                                    <div class="tooltip-wrap self-end">
+                                    <div class="self-end tooltip-wrap">
                                         <p class="text-sm text-red-400 ml-2.5">Email can't change</p>
                                         <div class="tooltip-content rounded-md mt-5 ml-2.5 lg:w-max">
                                             <p class="text-xs">You are sign in using {{ form.service }} service</p>
@@ -103,7 +103,7 @@
                     </form>
                 </ValidationObserver>
             </div>
-            <div class="modal hidden" id="modal">
+            <div class="hidden modal" id="modal">
                 <div class="modal-content">
                     <p class="text-center">Edit successful</p>
                     <button class="w-full bg-gray-200 hover:bg-gray-300 border border-gray-400 rounded-md py-1.5 mt-4">close</button>
