e8bc9e3c13ca44f2122767d27d4a4d6cd5e3884c
Amrilazim3 <a.azim0711@gmail.com>
2022-03-18 18:04:22 +0800

    adding link

--- components/VortechronInfo.vue
+++ components/VortechronInfo.vue
@@ -1,22 +1,31 @@
 <template>
     <ul class="inline-block xl:flex xl:ml-8 xl:space-x-8">
-        <NuxtLink 
-            to="/information/about"
-            :class="{'font-bold' : this.$nuxt.$route.path == '/information/about'}"
-        >
-            <li class="hover:text-gray-500 block">About us</li>
-        </NuxtLink>
-        <NuxtLink
-            to="/information/contact" 
-            :class="{'font-bold' : this.$nuxt.$route.path == '/information/contact'}"
-        >
+        <button @click.prevent="openAboutUs">
+            <li class="block hover:text-gray-500">About us</li>
+        </button>
+        <button @click.prevent="openContactUs">
             <li class="hover:text-gray-500">Contact Us</li>
-        </NuxtLink>
-        <NuxtLink 
-            to="/information/about-website"
-            :class="{'font-bold' : this.$nuxt.$route.path == '/information/about-website'}"
-        >
+        </button>
+        <button @click.prevent="openAboutWebsite">
             <li class="hover:text-gray-500" >About this Website</li>
-        </NuxtLink>
+        </button>
     </ul>
 </template>
+
+<script>
+export default {
+    methods: {
+        openAboutUs() {
+            window.open('https://github.com/Amrilazim3');
+        },
+
+        openContactUs() {
+            window.open('https://www.instagram.com/amrilazim_/?hl=ms');
+        },
+
+        openAboutWebsite() {
+            window.open('https://app.vortechron.com/information/about-website');
+        }
+    }
+}
+</script>
