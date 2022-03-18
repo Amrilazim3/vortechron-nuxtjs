cdbd2f84151c324076c1f5026100b9bf24510976
Amrilazim3 <a.azim0711@gmail.com>
2022-03-18 10:19:42 +0800

    Displaying posts count and paginate user posts

--- pages/user/account/profile/index.vue
+++ pages/user/account/profile/index.vue
@@ -22,7 +22,7 @@
             <div class="font-semibold mt-3.5 space-x-3.5 text-xl md:-mt-0 md:self-center hidden md:block">
                 <NuxtLink to="/user/followers">{{ followersCount }} followers</NuxtLink>
                 <NuxtLink to="/user/following">{{ followingCount }} following</NuxtLink>
-                <span>0 post</span>
+                <span>{{ postsCount }} post</span>
             </div>
         </div>
         <div>
@@ -37,13 +37,65 @@
         <div class="font-semibold mt-3.5 space-x-3.5 text-xl md:-mt-0 md:self-center md:hidden">
             <NuxtLink to="/user/followers">{{ followersCount }} followers</NuxtLink>
             <NuxtLink to="/user/following">{{ followingCount }} following</NuxtLink>
-            <span>0 post</span>
+            <span>{{ postsCount }} post</span>
         </div>
+
+        <main>
+            <div class="h-0.5 bg-white mt-8"></div>
+
+            <div v-if="noPost && $auth.user.email_verified_at || $auth.user.service">
+                <h1 class="text-center text-lg mt-52">No post yet, <NuxtLink to="/user/posts/create" class="text-blue-500 hover:underline">create your post</NuxtLink> now</h1>
+            </div>
+            <div v-else-if="noPost && !$auth.user.email_verified_at && !$auth.user.service">
+                <h1 class="text-center text-red-400 text-lg mt-44">Please verified your account to create a post</h1>
+            </div>
+
+            <template v-if="!noPost">
+                <section class="w-10/12 mt-4 mx-auto sm:grid sm:grid-cols-6 sm:w-full">
+                    <div class="relative px-2 py-1 mb-6 sm:mr-3 bg-white border rounded-md shadow-2xl hover:bg-gray-100 transition duration-300 ease-in-out sm:col-span-3 lg:col-span-2" 
+                        v-for="post in posts"
+                        :key="post.id"
+                    >
+                        <template v-if="!post.thumbnail">
+                            <div class="flex bg-gray-300 w-full h-32 mb-4">
+                                <p class="self-center mx-auto font-thin cursor-default">BLANK</p>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <img :src="post.thumbnail" alt="" class="w-full h-32 mb-4">
+                        </template>
+                        <template v-if="post.category[0]">
+                            <NuxtLink :to="`/user/posts/categories/${post.category_slug[0]}`" class="text-sm text-blue-400 hover:underline">{{ post.category[0] }}</NuxtLink>
+                        </template>
+                        <template v-else>
+                            <p class="text-sm opacity-0 cursor-default">hidden</p>
+                        </template>
+                        <NuxtLink :to="`/user/posts/${post.slug}`" class="flex text-lg font-bold mt-2.5 hover:underline">{{ post.title }}</NuxtLink>
+                        <p class="mt-4 text-sm h-20">{{ post.excerpt }}</p>
+                        <div class="pb-2 bottom-0 flex justify-between">
+                            <VueMomentsAgo prefix="posted" suffix="ago" :date="post.created_at" lang="en" class="self-center" />
+                            <button class="self-center bg-blue-300 px-4 py-1.5 border rounded-md hover:bg-blue-400" @click="$router.push(`/user/posts/${post.slug}`)">View</button>
+                        </div>
+                    </div>
+                    <div v-if="posts.length" v-observe-visibility="handleScrolledToBottom"></div>
+                </section>
+            </template>
+
+            <NuxtLink to="/user/posts/create" v-if="!noPost" class="fixed bottom-1.5 right-1.5 px-3.5 py-1 rounded-lg bg-blue-400 hover:bg-blue-500">
+                create post
+            </NuxtLink>
+        </main>
     </section>
 </template>
 
 <script>
+import VueMomentsAgo from 'vue-moments-ago'
+
 export default {
+    components: {
+        VueMomentsAgo
+    },
+
     layout: 'hide-subnav',
     
     middleware: 'auth',
@@ -56,13 +108,19 @@ export default {
     },
 
     mounted() {
+        this.getPosts(this.page);
         this.getFollowersAndFollowing();
     },
 
     data() {
         return {
             followersCount: 0,
-            followingCount: 0
+            followingCount: 0,
+            posts: [],
+            page: 1,
+            lastPage: 1,
+            postsCount: 0,
+            noPost: false,
         }
     },
 
@@ -74,6 +132,28 @@ export default {
                     this.followingCount = res.following_count;
                 })
         },
+
+        getPosts(page) {
+            this.$axios.$get(`/api/user/posts?page=${page}`)
+                .then(({ posts, posts_count }) => {
+                    this.postsCount = posts_count;
+                    this.lastPage = posts.meta.last_page;
+
+                    if (posts.data.length == 0) {
+                        this.noPost = true
+                    }
+
+                    this.posts.push(...posts.data);
+                })
+        },
+
+        handleScrolledToBottom(isVisible) {
+            if (!isVisible) { return }
+            if (this.page >= this.lastPage) { return }
+            this.page++;
+            this.getPosts(this.page);                
+        },
+
     }
 }
 </script>
