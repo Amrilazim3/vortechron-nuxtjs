b6a793e26a886f988cf7d5cb2037af0ee6e6b0b5
Amrilazim3 <a.azim0711@gmail.com>
2022-03-20 07:34:31 +0800

    adding sign out link in setting page

--- components/ProfileSideBarNav.vue
+++ components/ProfileSideBarNav.vue
@@ -3,5 +3,17 @@
         <NuxtLink to="/user/account/profile/edit-profile" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/profile/edit-profile' ? 'text-blue-500' : ''">Edit Profile</NuxtLink>
         <br>
         <NuxtLink to="/user/account/change-password" class="font-medium" :class="this.$nuxt.$route.path == '/user/account/change-password' ? 'text-blue-500' : ''">Change Password</NuxtLink>
+        <br>
+        <button class="mt-4 font-medium text-red-500 hover:underline" @click="signOut">sign out</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        signOut() {
+            this.$auth.logout();
+        }
+    }
+}
+</script>
