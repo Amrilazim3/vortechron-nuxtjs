9a7eaf17bcb5a9832f5b35a2da0bab3b8e4c92ae
Amrilazim3 <a.azim0711@gmail.com>
2022-03-19 10:25:35 +0800

    show all post associated by category

---
+++ pages/posts/categories/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="grid p-6 pt-56 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
+        <p>show all post by selected category</p>
+    </section>
+</template>
+
+<script>
+export default {
+    head: {
+        title: 'Categories',
+        meta: [
+            { hid: 'description', name: 'description', content: 'show categories' }
+        ],
+    },
+}
+</script>
