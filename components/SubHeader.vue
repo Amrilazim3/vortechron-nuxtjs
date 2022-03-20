c6633beff51e3c1e82d9543e53395ba2f8d4f58c
Amrilazim3 <a.azim0711@gmail.com>
2022-03-20 22:07:57 +0800

    commit changes

--- components/SubHeader.vue
+++ components/SubHeader.vue
@@ -5,7 +5,7 @@
                 <ul class="sm:flex sm:space-x-8">
                     <CategoryDropDown/>
                     
-                    <NuxtLink to="/">
+                    <NuxtLink to="/posts/latest" :class="$nuxt.$route.path == '/posts/latest' ? 'underline' : ''">
                         <li class="text-lg font-bold hover:underline">Latest</li>
                     </NuxtLink>
 
@@ -16,7 +16,7 @@
             </div>
             <div class="pt-2 sm:col-span-1 sm:mt-0 lg:col-span-2 xl:col-span-1 xl:ml-12 2xl:-ml-0">
                 <InputWithIcon>
-                    <img slot="icon" src="~/assets/search-icon.svg" alt="search-icon" class="absolute ml-3 mt-1 opacity-30 pointer-events-none">
+                    <img slot="icon" src="~/assets/search-icon.svg" alt="search-icon" class="absolute mt-1 ml-3 pointer-events-none opacity-30">
                     <input type="text" placeholder="Search users or posts" 
                         style="background: rgba(196, 196, 196, 1);"
                         class="pl-10 py-0.5 text-black placeholder-gray-500 rounded-lg border-none ring-2 ring-gray-400 w-2/5 sm:w-max focus:ring-gray-700 focus:ring-2 focus:outline-none"
@@ -24,7 +24,7 @@
                         @focus="search($event.target.value)"
                     >
                 </InputWithIcon>
-                <div class="absolute p-2 mt-2 z-10 bg-white rounded-md w-56" v-if="showUserResult">
+                <div class="absolute z-10 w-56 p-2 mt-2 bg-white rounded-md" v-if="showUserResult">
                     <div class="flex">
                         <p class="flex-1 text-center border-b-2 border-blue-400">users</p>
                         <p class="flex-1 text-center">posts</p>
@@ -34,12 +34,12 @@
                             <div class="flex py-1.5 hover:bg-gray-200">
                                 <template v-if="!user.image_url">
                                     <button class="bg-gray-300 p-2.5 rounded-full cursor-pointer" @click.prevent="$router.push(`/users/${user.id}`)">
-                                        <img class="h-4 w-4" src="~/assets/default-profile-icon.svg" alt="">
+                                        <img class="w-4 h-4" src="~/assets/default-profile-icon.svg" alt="">
                                     </button>
                                 </template>
                                 <template v-else>
                                     <button @click.prevent="$router.push(`/users/${user.id}`)">
-                                        <img class="h-8 w-8 rounded-full" :src="user.image_full_url" id="image-url">
+                                        <img class="w-8 h-8 rounded-full" :src="user.image_full_url" id="image-url">
                                     </button>
                                 </template>
                                 <NuxtLink :to="`/users/${user.id}`" class="self-center ml-2.5 w-9/12 truncate">{{ user.username }}</NuxtLink>
@@ -50,12 +50,12 @@
                             <div class="flex py-1.5 hover:bg-gray-200">
                                 <template v-if="!user.image_url">
                                     <button class="bg-gray-300 p-2.5 rounded-full cursor-pointer" @click.prevent="$router.push('/user/account/profile')">
-                                        <img class="h-4 w-4" src="~/assets/default-profile-icon.svg" alt="">
+                                        <img class="w-4 h-4" src="~/assets/default-profile-icon.svg" alt="">
                                     </button>
                                 </template>
                                 <template v-else>
                                     <button @click.prevent="$router.push('/user/account/profile')">
-                                        <img class="h-8 w-8 rounded-full" :src="user.image_full_url" id="image-url">
+                                        <img class="w-8 h-8 rounded-full" :src="user.image_full_url" id="image-url">
                                     </button>
                                 </template>
                                 <NuxtLink to="/user/account/profile" class="self-center ml-2.5 w-9/12 truncate">{{ user.username }}</NuxtLink>
@@ -63,7 +63,7 @@
                             <div class="h-0.5 w-full bg-gray-200"></div>
                         </template>
                     </div>
-                    <p v-if="noUserResult" class="text-black font-thin">no users found.</p>
+                    <p v-if="noUserResult" class="font-thin text-black">no users found.</p>
                 </div>
             </div>
         </div>
