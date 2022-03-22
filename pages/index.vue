d936e7adb1132c7a6568db7c5189e72e034fff38
Amrilazim3 <a.azim0711@gmail.com>
2022-03-22 16:31:12 +0800

    change page indent

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <div class="lg:flex">
       <div class="mt-40 lg:-mt-0 lg:flex-1 lg:self-center">
         <div class="lg:w-4/6 lg:ml-auto xl:w-3/5 2xl:w-7/12">
-          <h1 class="text-4xl text-center font-semibold lg:text-left 2xl:text-5xl">Daily Blog</h1>
-          <p class="text-normal text-center font-normal text-white lg:text-left 2xl:text-lg">
+          <h1 class="text-4xl font-semibold text-center lg:text-left 2xl:text-5xl">Daily Blog</h1>
+          <p class="font-normal text-center text-white text-normal lg:text-left 2xl:text-lg">
             <span class="text-2xl text-black">
               Explore
             </span>
