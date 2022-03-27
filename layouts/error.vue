a386ec7112f49772e7b39421060ff5938e9d5fbd
Amrilazim3 <a.azim0711@gmail.com>
2022-03-27 11:23:03 +0800

    commit changes

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="grid min-h-screen p-6 pt-56 sm:pt-44 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:pt-36 xl:max-w-5xl 2xl:max-w-7xl">
         <h1 class="self-end text-2xl font-bold text-center">OOPPPSSSSS!!!!, Seems like you've lost</h1>
-        <NuxtLink to="/dashboard" class="text-sm font-medium text-center text-blue-400 hover:underline">Go back.</NuxtLink>        
+        <button @click.prevent="$router.back()" class="font-medium text-blue-400  self-baseline hover:underline">Go back.</button>        
     </section>
 </template>
 
