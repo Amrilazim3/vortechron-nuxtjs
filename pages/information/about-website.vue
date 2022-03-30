c94ceea9fcf49f0beaeb26887818af77a9c5997f
Amrilazim3 <a.azim0711@gmail.com>
2022-03-31 07:43:54 +0800

    display basic information about vortechron

--- pages/information/about-website.vue
+++ pages/information/about-website.vue
@@ -1,11 +1,85 @@
 <template>
-    <section class="grid p-6 sm:justify-center sm:grid-cols-3 sm:max-w-xl sm:mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-7xl">
-        <h1>About website page</h1>
-    </section>
+    <div class="relative flex justify-center min-h-screen py-4 items-top dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <VortechronLogo class="h-8" />
+            </div>
+
+            <div class="mt-8 overflow-hidden bg-white shadow dark:bg-gray-800 sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                            <div class="ml-4 text-lg font-semibold leading-7"><a href="https://app.vortechron.com" class="text-gray-900 underline dark:text-white">Exploration</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                                We made this website special for you all for surfing blogs endlessly, read a posts as much as you want, create your own posts so other people around the world can see it too.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                            <div class="ml-4 text-lg font-semibold leading-7"><a href="https://app.vortechron.com" class="text-gray-900 underline dark:text-white">Categories Lists</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                                Find your favorite posts topic by categories we already made for you. So you can explore the posts you like.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                            <div class="ml-4 text-lg font-semibold leading-7"><span class="text-gray-900 underline dark:text-white">Friendship</span></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                                Vortechron blog also prepared a friendship system, you can follow other people you want. create more friends now!.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <div class="ml-4 text-lg font-semibold leading-7 text-gray-900 dark:text-white">Simple U.I</div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                                The main objective of this website is to focus on working with back-end system (Laravel) to perform many actions in this systems with api like (Oauth, third party service, Emaling, etc...), So it need more effort and time to design the page :(.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="ml-4 text-sm text-center text-gray-500 sm:text-right sm:ml-0">
+                    Vortechron - developed by amril azim.
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    
+
+    layout: 'hide-subnav',
+
+    head: {
+        title: '| About This Website',
+        meta: [
+            { hid: 'description', name: 'description', content: 'show website information' }
+        ],
+    },
 }
 </script>
